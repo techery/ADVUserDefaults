3dafdbce3ff131cd476c7c07ebfdc8fea15d3fbe
Natalia Grinevich <nata.grine@gmail.com>
2016-11-30 13:30:52 +0200

    Add carthage support

---
+++ UserDefaults/UserDefaults.h
@@ -0,0 +1,19 @@
+//
+//  UserDefaults.h
+//  UserDefaults
+//
+//  Created by Natalia Grinevich on 30.11.16.
+//  Copyright © 2016 Advantis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UserDefaults.
+FOUNDATION_EXPORT double UserDefaultsVersionNumber;
+
+//! Project version string for UserDefaults.
+FOUNDATION_EXPORT const unsigned char UserDefaultsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UserDefaults/PublicHeader.h>
+
+
